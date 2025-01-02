7da55a02ebf471251ff300d08ad57756185f790b
NijatHuseynzada <118692876+NijatHuseynzada@users.noreply.github.com>
2025-01-02 23:36:45 +0400

    Update api.js

--- api.js
+++ api.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:3000';
+const API_URL = 'http://localhost:3000';
 
-export const getRecipes = () => axios.get(`${API_BASE}/recipes`);
-export const createRecipe = (data) => axios.post(`${API_BASE}/recipes`, data);
-export const updateRecipe = (id, data) => axios.put(`${API_BASE}/recipes/${id}`, data);
-export const deleteRecipe = (id) => axios.delete(`${API_BASE}/recipes/${id}`);
-export const sendMessage = (data) => axios.post(`${API_BASE}/messages`, data);
+export const fetchRecipes = () => axios.get(${API_URL}/recipes);
+
+export const fetchRecipeById = (id) => axios.get(${API_URL}/recipes/${id});
+
+export const createRecipe = (data) => axios.post(${API_URL}/recipes, data);
+
+export const updateRecipe = (id, data) => axios.put(${API_URL}/recipes/${id}, data);
+
+export const deleteRecipe = (id) => axios.delete(${API_URL}/recipes/${id});
+
+export const sendMessage = (message) => axios.post(${API_URL}/messages, message);
